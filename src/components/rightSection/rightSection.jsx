fb9f2bf26d75539889ea0619f0d398545206851c
sankar-raul <raulsankar99@gmail.com>
2024-10-03 18:21:20 +0530

    1

--- src/components/rightSection/rightSection.jsx
+++ src/components/rightSection/rightSection.jsx
@@ -13,7 +13,7 @@ const RightSection = ({ state, handleWidth, handleRightCollapse }) => {
         handleWidth('right', rightRef.current.offsetWidth)
     }
     useEffect(() => {
-        setDisplay(prev => prev == "grid" ? "none" : "grid")
+        // setDisplay(prev => prev == "grid" ? "none" : "grid")
     }, [state])
     useEffect(() => {
             // alert()

2a2783ab9198d286118953b726571d6c7c11686d
sankar-raul <raulsankar99@gmail.com>
2024-11-25 02:56:00 +0530

    test

--- src/api/getLyrics.js
+++ src/api/getLyrics.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 export const getLyrics = async (song) => {
-    const apiEndPoint = (songName) => `/api/lyrics?q=${songName}`
+    const apiEndPoint = (songName) => `https://api.textyl.co/api/lyrics?q=${songName}`
     const data = await axios.get(apiEndPoint(song))
+    // console.log(data.data)
     return data.data
 }
 export default getLyrics

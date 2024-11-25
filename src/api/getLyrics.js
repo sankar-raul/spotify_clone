86d067cdc6d67f307e9446def4540af35ebd023f
sankar-raul <raulsankar99@gmail.com>
2024-11-26 01:49:13 +0530

    up

--- src/api/getLyrics.js
+++ src/api/getLyrics.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export const getLyrics = async (song) => {
     const apiEndPoint = (songName) => `/api/lyrics?q=${songName}`
-    const data = await axios.get(apiEndPoint(song))
+    const data = await axios.get(apiEndPoint(song), {
+        mode: 'no-cors',
+    })
     // console.log(data.data)
     return data.data
 }

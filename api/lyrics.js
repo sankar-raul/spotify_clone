367097d07c1a5f2421c325d0c4c514ea88509dfa
sankar-raul <raulsankar99@gmail.com>
2024-12-19 16:15:16 +0530

    test

--- api/lyrics.js
+++ api/lyrics.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { q } = req.query; // Extract query string from request
-  res.end(JSON.stringify(q));
-//   try {
-//     const response = await axios.get(`https://api.textyl.co/api/lyrics`, {
-//       params: { q },
-//     });
-//     res.status(200).json(response.data); // Send back the data
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   }
+  try {
+    const response = await axios.get(`https://api.textyl.co/api/lyrics`, {
+      params: { q },
+    });
+    res.status(200).json(response.data); // Send back the data
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 }
